eb8e0fbc22c9dc3960b8b75c76291d07039509e8
菲利 <wangzhe_nju@qq.com>
2025-02-12 02:13:47 +0800

    add framework product

---
+++ UJourneySDK.xcframework/ios-arm64_x86_64-simulator/UJourneySDK.framework/Headers/UJourneySDK.h
@@ -0,0 +1,18 @@
+//
+//  UJourneySDK.h
+//  UJourneySDK
+//
+//  Created by 王哲 on 2025/2/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UJourneySDK.
+FOUNDATION_EXPORT double UJourneySDKVersionNumber;
+
+//! Project version string for UJourneySDK.
+FOUNDATION_EXPORT const unsigned char UJourneySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UJourneySDK/PublicHeader.h>
+
+
